16ac3a51900cc83d2374da08da45502cf785bda4
赵豪哲 <tn38i31193@163.com>
2023-07-18 17:34:19 +0800

    保存

--- src/main/java/com/personnel/common/dto/UserInformationDto.java
+++ src/main/java/com/personnel/common/dto/UserInformationDto.java
@@ -1,5 +1,7 @@
 package com.personnel.common.dto;
 
+import com.alibaba.excel.annotation.format.DateTimeFormat;
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.personnel.common.base.dto.BaseDto;
 import lombok.Getter;
 import lombok.Setter;
@@ -26,52 +28,53 @@ public class UserInformationDto extends BaseDto {
     /**
      * 姓名
      */
-    @NotBlank(message = "姓名不能为空")
+    @NotBlank(message = "姓名不能为空;")
     private String name;
 
     /**
      * 姓名全拼
      */
-    @NotBlank(message = "姓名全拼不能为空")
+    @NotBlank(message = "姓名全拼不能为空;")
     private String nameSpelling;
 
     /**
      * 性别
      */
-    @NotBlank(message = "性别不能为空")
+    @NotBlank(message = "性别不能为空;")
     private String gender;
 
     /**
      * 身份证件类型：居民身份证、士官证、学生证、驾驶证、护照、港澳通行证
      */
-    @NotBlank(message = "身份证件类型不能为空")
+    @NotBlank(message = "身份证件类型不能为空;")
     private String identityCardType;
 
     /**
      * 身份证件号码
      */
-    @NotBlank(message = "身份证件号码不能为空")
+    @NotBlank(message = "身份证件号码不能为空;")
     private String identityCardNumber;
 
     /**
      * 出生日期
      */
-    @NotNull(message = "生日不能为空")
+    @NotNull(message = "生日不能为空;")
+    @JsonFormat(locale="zh", timezone="GMT+8", pattern="yyyy-MM-dd")
     private Date birthday;
 
     /**
      * 手机号码
      */
-    @NotBlank(message = "手机号码不能为空")
-    @Length(min = 11, max = 11, message = "手机号只能为11位")
-    @Pattern(regexp = "^[1][0-9]{10}$", message = "手机号格式有误")
+    @NotBlank(message = "手机号码不能为空;")
+    @Length(min = 11, max = 11, message = "手机号只能为11位;")
+    @Pattern(regexp = "^[1][0-9]{10}$", message = "手机号格式有误;")
     private String phone;
 
     /**
      * 电子邮箱
      */
-    @NotBlank(message = "邮件不能为空")
-    @Pattern(regexp ="^\\w+([-+.]\\w+)*@\\w+([-.]\\w+)*\\.\\w+([-.]\\w+)*$",message = "邮箱格式有误")
+    @NotBlank(message = "邮件不能为空;")
+    @Pattern(regexp ="^\\w+([-+.]\\w+)*@\\w+([-.]\\w+)*\\.\\w+([-.]\\w+)*$",message = "邮箱格式有误;")
     private String email;
 
 }
